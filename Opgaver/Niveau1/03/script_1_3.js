96967d68e5982adfcae96bd0a4c8aac47aa9ea4b
Phong <phongleduc@gmail.com>
2025-01-30 13:40:07 +0100

    alle opgaver i en mappe

--- Opgaver/Niveau1/03/script_1_3.js
+++ Opgaver/Niveau1/03/script_1_3.js
@@ -11,10 +11,34 @@
 // }
 
 // hjælp fra emilie
-function skjulElementViaCssSelector(cssSelector) {
-    if (document.querySelector(cssSelector)) {
-      document.querySelector(cssSelector).style.display = "none";
-    }
-  }
+// function skjulElementViaCssSelector(cssSelector) {
+//     if (document.querySelector(cssSelector)) {
+//       document.querySelector(cssSelector).style.display = "none";
+//     }
+//   }
    
 //   skjulElementViaCssSelector(".container2");
+
+
+
+function skjulElementViaCssSelector (CssSelector) {
+  let element = document.querySelector(CssSelector)
+  skjulElement(element)
+
+}
+
+function skjulElementerViaCssSelector (CssSelector) {
+  let elements = document.querySelectorAll(CssSelector)
+  elements.foreach(function(element) {
+    skjulElement(element)
+
+  })
+}
+
+
+function skjulElement (element) {
+  if (element != null) element.style.display = "none"
+}
+
+skjulElementViaCssSelector(".bodycopy")
+skjulElementViaCssSelector(".headline")
